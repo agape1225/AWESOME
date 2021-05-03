39d2e690cd3c664775f826533de254ab390c9acc
wkdgns135 <82528291+wkdgns135@users.noreply.github.com>
2021-05-03 16:54:49 +0900

    123

---
+++ src/main/webapp/resources/js/script.js
@@ -0,0 +1,32 @@
+
+
+const video = document.getElementById('video')
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/resource/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/resource/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/resource/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/resource/models')
+]).then(startVideo)
+
+function startVideo() {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    
+  }, 100)
+})
