e5ba3e57d78e8a02428f8b0c22d2d7012e037645
agape1225 <49804691+agape1225@users.noreply.github.com>
2021-05-03 17:39:03 +0900

    problem solved

--- src/main/webapp/resources/js/script.js
+++ src/main/webapp/resources/js/script.js
@@ -3,10 +3,10 @@
 const video = document.getElementById('video')
 
 Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/resource/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/resource/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/resource/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/resource/models')
+  faceapi.nets.tinyFaceDetector.loadFromUri('/resources/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/resources/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/resources/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/resources/models')
 ]).then(startVideo)
 
 function startVideo() {
@@ -27,6 +27,7 @@ video.addEventListener('play', () => {
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
     canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
     faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    console.log(detections)
     
   }, 100)
 })
